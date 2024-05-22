8e4c232dbbec53be910c2bd639e8a65f4ea9cc7c
William Tran <wtran29@gmail.com>
2024-05-22 12:28:00 -0700

    implement random string function

--- tools.go
+++ tools.go
@@ -1,5 +1,23 @@
 package toolbox
 
-type Tools struct {
-	
+import (
+	"crypto/rand"
+	"math/big"
+)
+
+var randomRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+_1234567890")
+
+// Tools is the type used to instantiate module. Any variable of this type
+// will have access to the methods with receiver *Tools.
+type Tools struct{}
+
+// RandomString generates a random string of length using characters from randomRunes
+func (t *Tools) RandomString(n int) string {
+	runes := []rune(randomRunes)
+	result := make([]rune, n)
+	for i := range result {
+		num, _ := rand.Int(rand.Reader, big.NewInt(int64(len(runes))))
+		result[i] = runes[num.Int64()]
+	}
+	return string(result)
 }
