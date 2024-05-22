3842b63153239e8c18a371069bba10ed48762e51
William Tran <wtran29@gmail.com>
2024-05-22 12:51:29 -0700

    add random string test

---
+++ tools_test.go
@@ -0,0 +1,13 @@
+package toolbox
+
+import "testing"
+
+func TestTools_RandomString(t *testing.T) {
+	var testTools Tools
+
+	s := testTools.RandomString(10)
+	if len(s) != 10 {
+		t.Errorf("wrong length. wanted=%d, got=%d", 10, len(s))
+	}
+
+}
